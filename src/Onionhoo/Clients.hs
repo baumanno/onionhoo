35e2532d8f7ce980bfd51defe8e8411afc3407e2
Oliver Baumann <baumanno@cip.ifi.lmu.de>
2016-03-08 23:56:00 +0100

    Clients document
    Implement the Onionoo clients document.

---
+++ src/Onionhoo/Clients.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | A Clients-document for bridges and relays
+module Onionhoo.Clients (Clients) where
+
+import qualified Onionhoo.Clients.Bridge as B (Bridge)
+
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Aeson.Types
+import Data.Text (Text)
+
+-- | A Clients document containing information on bridges and relays
+data Clients =
+  Clients {version :: Text -- ^ the current API version
+          ,nextMajorVersionScheduled :: Maybe Text -- ^ optional
+          ,relaysPublished :: Text -- ^ UTC date when the list of relays was published
+          ,relays :: Array -- ^ list of relays
+          ,bridgesPublished :: Text -- ^ UTC date when the list of bridges was published
+          ,bridges :: [B.Bridge]} -- ^ list of bridges
+  deriving (Show)
+
+$(deriveFromJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
+                 ''Clients)
---
+++ src/Onionhoo/Clients/Bridge.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | A Clients document for bridges
+module Onionhoo.Clients.Bridge (Bridge, ClientHistory) where
+
+import Onionhoo.History.Graph
+import Control.Monad (mzero)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Aeson.Types
+import Data.Text (Text)
+
+-- | Contains Clients information for a bridge
+data Bridge =
+  Bridge {fingerprint :: Text
+         ,averageClients :: Maybe ClientHistory}
+  deriving (Show)
+
+data ClientHistory =
+  ClientHistory {oneWeek :: Maybe Graph
+                ,oneMonth :: Maybe Graph
+                ,threeMonths :: Maybe Graph
+                ,oneYear :: Maybe Graph
+                ,fiveYears :: Maybe Graph}
+  deriving (Show)
+
+$(deriveFromJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
+                 ''Bridge)
+
+instance FromJSON ClientHistory where
+  parseJSON (Object v) = 
+    ClientHistory <$>
+    v .:? "1_week" <*>
+    v .:? "1_month" <*>
+    v .:? "3_months" <*>
+    v .:? "1_year" <*>
+    v .:? "5_years"
+  parseJSON _ = mzero
